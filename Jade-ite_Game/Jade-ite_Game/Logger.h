2f89583f75cbe6b04994d85184216886e44442e3
Kouniakeia-Koinwnia <nikkalaitsidis10@gmail.com>
2026-07-06 20:58:20 +0200

    Added a Logger for Debugging, plus Colors.h

--- Jade-ite_Game/Jade-ite_Game/Logger.h
+++ Jade-ite_Game/Jade-ite_Game/Logger.h
@@ -1,13 +1,17 @@
 #pragma once
-
+#include <string>
 #include <string_view>
 #include <source_location>
 
 class Logger
 {
 private:
+	static std::string CurrentDate();
 
 public:
+	Logger() {};
+	~Logger() = default;
 
-
+	static void Log(const std::string_view message);
+	static void Error(const std::string_view, std::source_location = std::source_location::current());
 };
